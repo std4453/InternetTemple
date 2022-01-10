ac0046b834d2c9849c0626034d429d0c29a2359d
std4453 <me@std4453.com>
2022-01-11 00:48:09 +0800

    Scroll to bottom at start

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,19 +1,25 @@
 import { ScrollContext, useScroll } from "components/useOnScroll";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./index.module.css";
 
 export default function Wrapper({ children, numPages }) {
   const outerRef = useRef(null);
   const innerRef = useRef(null);
 
+  const [scrollInit, setScrollInit] = useState(false);
+  useEffect(() => {
+    outerRef.current.scrollTo(0, (numPages - 1) * window.innerHeight);
+    setScrollInit(true);
+  }, []);
   const [value, updateScroll] = useScroll();
   const handleScroll = useCallback(() => {
+    if (!scrollInit) return;
     const innerHeight = window.innerHeight * numPages;
     const scrollBottom =
       innerHeight - (outerRef.current.scrollTop + window.innerHeight);
     const value = (scrollBottom / window.innerWidth) * 100;
     updateScroll(value);
-  }, [updateScroll, numPages]);
+  }, [updateScroll, numPages, scrollInit]);
   useEffect(() => {
     const listener = () => {
       handleScroll();
